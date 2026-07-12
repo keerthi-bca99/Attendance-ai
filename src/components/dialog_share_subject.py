89cdfedfaaaea064e82f2e75cbb64072dfc48a00
keerthi-bca99 <code.n.keerthi@gmail.com>
2026-07-12 22:45:50 +0530

    Update dialog_share_subject.py

--- src/components/dialog_share_subject.py
+++ src/components/dialog_share_subject.py
@@ -6,7 +6,7 @@ import io
 
 @st.dialog("Share Class Link")
 def share_subject_dialog(subject_name, subject_code):
-    app_domain = "class-web.streamlit.app"
+    app_domain = "class-webs.streamlit.app"
     join_url = f"{app_domain}/?join-code={subject_code}"
 
     st.header("Scan to Join")
